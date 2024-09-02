b83ee534f3cef8a000d4142ceb29a022fc248183
Rohit Manohar <manoharrohit372@gmail.com>
2024-09-02 19:05:34 +0530

    done

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -8,22 +8,22 @@ const Portfolio = () => {
       <h1 className="text-white text-3xl mb-4">Portfolio</h1>
 
       {/* Navigation Bar */}
-      <div className="flex justify-start space-x-4 mb-8">
+      <div className="flex space-x-4 mb-8">
         <button
           onClick={() => setActiveTab('All')}
-          className="px-4 py-2 rounded-full font-semibold bg-black/30 text-gray-300  hover:text-amber-500 transition-colors duration-300"
+          className={`px-4 py-2 rounded-full bg-black/30 ${activeTab === 'All' ? 'text-amber-500' : 'text-gray-300'} hover:text-amber-400 transition-colors duration-300`}
         >
           All
         </button>
         <button
           onClick={() => setActiveTab('Web Development')}
-          className="px-4 py-2 rounded-full font-semibold bg-black/30 text-gray-300  hover:text-amber-500 transition-colors duration-300"
+          className={`px-4 py-2 rounded-full bg-black/30 ${activeTab === 'Web Development' ? 'text-amber-500' : 'text-gray-300'} hover:text-amber-400 transition-colors duration-300`}
         >
           Web Development
         </button>
         <button
           onClick={() => setActiveTab('Web Design')}
-          className="px-4 py-2 rounded-full font-semibold bg-black/30 text-gray-300  hover:text-amber-500 transition-colors duration-300"
+          className={`px-4 py-2 rounded-full bg-black/30 ${activeTab === 'Web Design' ? 'text-amber-500' : 'text-gray-300'} hover:text-amber-400 transition-colors duration-300`}
         >
           Web Design
         </button>
