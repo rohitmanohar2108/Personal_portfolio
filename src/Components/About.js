f49cdc472e2ab91b01057d60cec00fd519de53f7
Rohit Manohar <manoharrohit372@gmail.com>
2024-09-03 00:01:18 +0530

    done

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
 import { gsap } from "gsap";
-import { FaCode, FaPaintBrush, FaCogs, FaLaptopCode } from "react-icons/fa"; // Import the icons you need
+import { FaCode, FaPaintBrush, FaCogs, FaLaptopCode } from "react-icons/fa";
 import { FcAbout } from "react-icons/fc";
 import CircularTimeline from "./ CircularTimeline";
+
 const About = () => {
   useEffect(() => {
     // Animate the header and paragraphs
@@ -22,7 +23,7 @@ const About = () => {
 
   return (
     <div>
-      <div className="flex items-center ">
+      <div className="flex items-center">
         <h1 className="text-white text-3xl font-courier-prime about-header">
           About Me
         </h1>
@@ -31,7 +32,7 @@ const About = () => {
 
       <hr className="w-32 border-2 border-yellow-600 mb-6 mt-3 rounded-full about-header" />
 
-      <p className="text-white text-sm font-courier-prime ">
+      <p className="text-white text-sm font-courier-prime">
         I am a Computer Science Student at NITK. I have a deep passion for
         coding and constantly strive to enhance my skills by learning new
         technologies. I love tackling challenging problems and finding
@@ -40,7 +41,7 @@ const About = () => {
         projects and learn from others in the field.
       </p>
 
-      <p className="text-white text-sm mt-4 font-courier-prime ">
+      <p className="text-white text-sm mt-4 font-courier-prime">
         When I am not coding, I enjoy exploring the latest tech trends and
         participating in hackathons and coding competitions. Let's connect and
         create something amazing together! Whether it's building a new app,
@@ -49,57 +50,89 @@ const About = () => {
         tech community.
       </p>
 
-      <h2 className="text-amber-400 text-4xl mt-4  font-extrabold font-courier-prime">
+      <h2 className="text-amber-400 text-4xl mt-4 font-extrabold font-courier-prime">
         What I'm Doing
       </h2>
 
       {/* Skill Boxes Section */}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
         {/* Box 1: Web Design */}
-        <div className="bg-purple-800/50 p-6 rounded-lg flex flex-col items-center text-center skill-box font-courier-prime ">
-          <FaPaintBrush className="text-amber-500 text-5xl mb-3" />
-          <h3 className="text-white text-2xl mb-2">Web Design</h3>
-          <p className="text-gray-400 text-sm">
-            Creating visually appealing designs and user interfaces for
-            websites. Expertise in UI/UX principles, responsive design, and
-            modern design tools.
-          </p>
+        <div className="relative p-6 rounded-lg flex flex-col items-center text-center skill-box font-courier-prime group overflow-hidden">
+          <div
+            className="absolute inset-0 bg-cover bg-center opacity-90"
+            style={{ backgroundImage: "url('https://www.exovia.de/wp-content/uploads/2021/04/was-ist-webdesign-einfuehrung.jpg')" }}
+          ></div>
+          <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-70 transition-opacity duration-300"></div>
+          <div className="relative z-10 flex flex-col items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <FaPaintBrush className="text-amber-500 text-5xl mb-3" />
+            <h3 className="text-white text-2xl mb-2">Web Design</h3>
+            <p className="text-white text-sm">
+              Creating visually appealing designs and user interfaces for
+              websites. Expertise in UI/UX principles, responsive design, and
+              modern design tools.
+            </p>
+          </div>
         </div>
 
         {/* Box 2: Web Development */}
-        <div className="bg-purple-800/50  p-6 rounded-lg flex flex-col items-center text-center skill-box font-courier-prime ">
-          <FaLaptopCode className="text-amber-500 text-5xl mb-3" />
-          <h3 className="text-white text-2xl mb-2">Web Development</h3>
-          <p className="text-gray-400 text-sm">
-            Building and maintaining websites using various technologies such as
-            HTML, CSS, JavaScript, and frameworks like React. Focus on
-            performance and scalability.
-          </p>
+        <div className="relative p-6 rounded-lg flex flex-col items-center text-center skill-box font-courier-prime group overflow-hidden">
+          <div
+            className="absolute inset-0 bg-cover bg-center opacity-90"
+            style={{ backgroundImage: "url('https://www.simplilearn.com/ice9/free_resources_article_thumb/is_web_development_good_career.jpg')" }}
+          ></div>
+          <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-70 transition-opacity duration-300"></div>
+          <div className="relative z-10 flex flex-col items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <FaLaptopCode className="text-amber-500 text-5xl mb-3" />
+            <h3 className="text-white text-2xl mb-2">Web Development</h3>
+            <p className="text-white text-sm">
+              Building and maintaining websites using various technologies such as
+              HTML, CSS, JavaScript, and frameworks like React. Focus on
+              performance and scalability.
+            </p>
+          </div>
         </div>
 
         {/* Box 3: Software Development */}
-        <div className="bg-purple-800/50  p-6 rounded-lg flex flex-col items-center text-center skill-box font-courier-prime ">
-          <FaCode className="text-amber-500 text-5xl mb-3" />
-          <h3 className="text-white text-2xl  mb-2">Software Development</h3>
-          <p className="text-gray-400 text-sm">
-            Developing software solutions with a focus on algorithms, data
-            structures, and code optimization. Proficient in languages like Java
-            and C++.
-          </p>
+        <div className="relative p-6 rounded-lg flex flex-col items-center text-center skill-box font-courier-prime group overflow-hidden">
+          <div
+            className="absolute inset-0 bg-cover bg-center opacity-90"
+            style={{ backgroundImage: "url('https://okcredit-blog-images-prod.storage.googleapis.com/2021/03/Software-Development-Business1--1-.jpg')" }}
+          ></div>
+          <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-70 transition-opacity duration-300"></div>
+          <div className="relative z-10 flex flex-col items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <FaCode className="text-amber-500 text-5xl mb-3" />
+            <h3 className="text-white text-2xl mb-2">Software Development</h3>
+            <p className="text-white text-sm">
+              Developing software solutions with a focus on algorithms, data
+              structures, and code optimization. Proficient in languages like Java
+              and C++.
+            </p>
+          </div>
         </div>
 
         {/* Box 4: Front-End */}
-        <div className="bg-purple-800/50  p-6 rounded-lg flex flex-col items-center text-center skill-box font-courier-prime ">
-          <FaCogs className="text-amber-500 text-5xl mb-3" />
-          <h3 className="text-white text-2xl  mb-2">Front-End</h3>
-          <p className="text-gray-400 text-sm">
-            Specializing in creating interactive and dynamic user interfaces.
-            Experienced with frameworks such as React, ensuring a smooth and
-            engaging user experience.
-          </p>
+        <div className="relative p-6 rounded-lg flex flex-col items-center text-center skill-box font-courier-prime group overflow-hidden">
+          <div
+            className="absolute inset-0 bg-cover bg-center opacity-90"
+            style={{ backgroundImage: "url('https://media.licdn.com/dms/image/D5612AQFfhTEictqBHA/article-cover_image-shrink_720_1280/0/1716965604025?e=2147483647&v=beta&t=QQbiRZaZNurRKVw6bW1J0hG5x2f-7PMAowmfdFF3Dz4')" }}
+          ></div>
+          <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-70 transition-opacity duration-300"></div>
+          <div className="relative z-10 flex flex-col items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <FaCogs className="text-amber-500 text-5xl mb-3" />
+            <h3 className="text-white text-2xl mb-2">Front-End</h3>
+            <p className="text-white text-sm">
+              Specializing in creating interactive and dynamic user interfaces.
+              Experienced with frameworks such as React, ensuring a smooth and
+              engaging user experience.
+            </p>
+          </div>
         </div>
-        <p className="text-amber-400 text-4xl mt-4  font-extrabold font-courier-prime">My Skills</p>
+
+        <p className="text-amber-400 text-4xl mt-4 font-extrabold font-courier-prime">
+          My Skills
+        </p>
       </div>
+
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-6">
         <CircularTimeline
           percentage={90}
@@ -109,7 +142,7 @@ const About = () => {
         <CircularTimeline
           percentage={50}
           title="Data Structures & Algorithms"
-          description="Decent problem-solving using DSA, with experience Programming"
+          description="Decent problem-solving using DSA, with experience in programming."
         />
         <CircularTimeline
           percentage={75}
