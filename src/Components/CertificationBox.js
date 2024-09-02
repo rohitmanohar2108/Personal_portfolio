c6ac1d02931478f634de967c24a411daaa3568d0
Rohit Manohar <manoharrohit372@gmail.com>
2024-09-03 00:47:57 +0530

    done

--- src/Components/CertificationBox.js
+++ src/Components/CertificationBox.js
@@ -7,7 +7,7 @@ const CertificationBox = ({ logo, title, description, link }) => {
       href={link} 
       target="_blank" 
       rel="noopener noreferrer" 
-      className="relative p-6 rounded-lg flex flex-col items-center text-center certification-box font-courier-prime group overflow-hidden bg-white bg-opacity-30"
+      className="relative p-6 rounded-lg flex flex-col items-center text-center certification-box font-courier-prime group overflow-hidden bg-white bg-opacity-30 transition-transform transform hover:scale-105"
     >
       <div className="relative w-full h-24 mb-4">
         <img
@@ -16,10 +16,10 @@ const CertificationBox = ({ logo, title, description, link }) => {
           className="absolute inset-0 w-full h-full object-contain object-center"
         />
       </div>
-      <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-30 transition-opacity duration-300"></div>
-      <div className="relative z-10 flex flex-col items-center opacity-100 transition-opacity duration-300">
+
+      <div className="relative z-10 flex flex-col items-center opacity-100 transition-opacity duration-300 ">
         
-        <h3 className="text-black text-xl mb-2 ">{title}</h3>
+        <h3 className="text-white text-xl mb-2 ">{title}</h3>
        
       </div>
     </a>
