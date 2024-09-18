d1c6f9bcad052b7a0d16021ad3eaffc47080de0c
Rohit Manohar <manoharrohit372@gmail.com>
2024-09-18 18:40:10 +0530

    vhvhfu

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -47,38 +47,25 @@ const Home = () => {
         className="w-full p-6 rounded-lg"
         style={{
           background: `
-            linear-gradient(135deg, rgba(15, 23, 42, 0.8) 0%, rgba(21, 32, 43, 0.9) 100%),
-            url('https://www.transparenttextures.com/patterns/dark-mosaic.png')`,
-          backgroundBlendMode: 'overlay',
-          backgroundSize: 'cover',
-          color: 'white',
+            radial-gradient(circle at center, rgba(190, 24, 93, 0.4) 10%, rgba(18, 18, 18, 1) 30%),
+            radial-gradient(circle at center, rgba(255, 105, 180, 0.5) 10%, rgba(18, 18, 18, 1) 30%)`,
+          backgroundSize: "100% 50%, 100% 90%",
         }}
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
       >
         {/* Top-right Navigation */}
         <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 -my-4 justify-end relative z-20">
         <button
             onClick={() => handleSectionChange("About")}
-            className={`px-4 py-2 rounded-full  bg-black border border-blue-900 hover:border-blue-500 text-base ${
-              currentSection === "About" ? "text-blue-600" : "text-white"
-            } hover:text-blue-500 font-dm-sans transition duration-300 ease-in-out transform hover:scale-105`}
+            className={`px-4 py-2 rounded-full  bg-zinc-900 border border-zinc-800 hover:border-white text-base ${
+              currentSection === "About" ? "text-rose-600" : "text-white"
+            } hover:text-rose-600 font-dm-sans transition duration-300 ease-in-out transform hover:scale-105`}
           >
             About
           </button>
 
           <button
             onClick={() => handleSectionChange("Resume")}
-            className={`px-4 py-2 rounded-full bg-black border border-zinc-800 hover:border-white text-base ${
+            className={`px-4 py-2 rounded-full bg-zinc-900 border border-zinc-800 hover:border-white text-base ${
               currentSection === "Resume" ? "text-rose-600" : "text-white"
             } hover:text-rose-600 font-dm-sans transition duration-300 ease-in-out transform hover:scale-105 `}
           >
@@ -86,7 +73,7 @@ const Home = () => {
           </button>
           <button
             onClick={() => handleSectionChange("Portfolio")}
-            className={`px-4 py-2 rounded-full bg-black border border-zinc-800 hover:border-white text-base ${
+            className={`px-4 py-2 rounded-full bg-zinc-900 border border-zinc-800 hover:border-white text-base ${
               currentSection === "Portfolio" ? "text-rose-600" : "text-white"
             } hover:text-rose-600 font-dm-sans transition duration-300 ease-in-out transform hover:scale-105`}
           >
@@ -94,7 +81,7 @@ const Home = () => {
           </button>
           <button
             onClick={() => handleSectionChange("Blog")}
-            className={`px-4 py-2 rounded-full bg-black border border-zinc-800 hover:border-white text-base ${
+            className={`px-4 py-2 rounded-full bg-zinc-900 border border-zinc-800 hover:border-white text-base ${
               currentSection === "Blog" ? "text-rose-600" : "text-white"
             } hover:text-rose-600 font-dm-sans transition duration-300 ease-in-out transform hover:scale-105`}
           >
@@ -103,7 +90,7 @@ const Home = () => {
 
           <button
             onClick={() => handleSectionChange("Contact")}
-            className={`px-4 py-2 rounded-full bg-black border border-zinc-800 hover:border-white text-base ${
+            className={`px-4 py-2 rounded-full bg-zinc-900 border border-zinc-800 hover:border-white text-base ${
               currentSection === "Contact" ? "text-rose-600" : "text-white"
             } hover:text-rose-600 font-dm-sans transition duration-300 ease-in-out transform hover:scale-105`}
           >
