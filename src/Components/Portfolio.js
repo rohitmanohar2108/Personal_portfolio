21b5324e79e59a7e00ea09a21dd0da76da6d1718
Rohit Manohar <manoharrohit372@gmail.com>
2024-09-18 17:40:32 +0530

    done

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -12,21 +12,15 @@ const projects = {
     },
     {
       id: 2,
-      image: 'https://vercel.com/_next/image?url=%2Fapi%2Fscreenshot%3Fdark%3D1%26deploymentId%3Ddpl_33ipk61D9T6AhM1YK67rmpvPzvjG%26teamId%3Drohits-projects-1ff89b20%26withStatus%3D1&w=1920&q=75&dpl=dpl_Hp88LwrD6GmyCpS3RttV5rbznXBX',
+      image: 'https://vercel.com/_next/image?url=%2Fapi%2Fscreenshot%3Fdark%3D0%26deploymentId%3Ddpl_33ipk61D9T6AhM1YK67rmpvPzvjG%26teamId%3Drohits-projects-1ff89b20%26withStatus%3D1&w=1920&q=75&dpl=dpl_2q5xmLHavNwyLXRhZZV9GC6pWhpd',
       title: 'Portfolio',
       description: 'Web Design',
       link: 'https://ro-portfolio-olive.vercel.app/'
     },
-    {
-      id: 3,
-      image: 'https://vercel.com/_next/image?url=%2Fapi%2Fscreenshot%3Fdark%3D1%26deploymentId%3Ddpl_EYKMtAYEXbrN9b2JxsWvaSKt2eD6%26teamId%3Drohits-projects-1ff89b20%26withStatus%3D1&w=1920&q=75&dpl=dpl_Hp88LwrD6GmyCpS3RttV5rbznXBX',
-      title: 'NewScope',
-      description: 'Web Dev',
-      link: 'https://newscope.vercel.app/'
-    },
+    
     {
       id: 4,
-      image: 'https://vercel.com/_next/image?url=%2Fapi%2Fscreenshot%3Fdark%3D1%26deploymentId%3Ddpl_HjSPgwA6Etfb94QkbfDfBHaj9oUe%26teamId%3Drohits-projects-1ff89b20%26withStatus%3D1&w=1920&q=75&dpl=dpl_Hp88LwrD6GmyCpS3RttV5rbznXBX',
+      image: 'https://vercel.com/_next/image?url=%2Fapi%2Fscreenshot%3Fdark%3D1%26deploymentId%3Ddpl_7Vf2Yu6pjUiVbHonAqNRZAqLqie3%26teamId%3Drohits-projects-1ff89b20%26withStatus%3D1&w=1920&q=75&dpl=dpl_2q5xmLHavNwyLXRhZZV9GC6pWhpd',
       title: 'Personel portfolio',
       description: 'Web Dev',
       link: 'https://personal-portfolio-kappa-teal.vercel.app/'
@@ -40,26 +34,20 @@ const projects = {
       description: 'Web Dev',
       link: '#'
     },
-    {
-      id: 2,
-      image: 'https://vercel.com/_next/image?url=%2Fapi%2Fscreenshot%3Fdark%3D1%26deploymentId%3Ddpl_EYKMtAYEXbrN9b2JxsWvaSKt2eD6%26teamId%3Drohits-projects-1ff89b20%26withStatus%3D1&w=1920&q=75&dpl=dpl_Hp88LwrD6GmyCpS3RttV5rbznXBX',
-      title: 'NewScope',
-      description: 'Web Dev ',
-      link: 'https://newscope.vercel.app/'
-    }
+    
   ],
   
   'Web Design': [
     {
       id: 1,
-      image: 'https://vercel.com/_next/image?url=%2Fapi%2Fscreenshot%3Fdark%3D1%26deploymentId%3Ddpl_33ipk61D9T6AhM1YK67rmpvPzvjG%26teamId%3Drohits-projects-1ff89b20%26withStatus%3D1&w=1920&q=75&dpl=dpl_Hp88LwrD6GmyCpS3RttV5rbznXBX',
+      image: 'https://vercel.com/_next/image?url=%2Fapi%2Fscreenshot%3Fdark%3D0%26deploymentId%3Ddpl_33ipk61D9T6AhM1YK67rmpvPzvjG%26teamId%3Drohits-projects-1ff89b20%26withStatus%3D1&w=1920&q=75&dpl=dpl_2q5xmLHavNwyLXRhZZV9GC6pWhpd',
       title: 'Portfolio',
       description: ' Web Design',
       link: 'https://ro-portfolio-olive.vercel.app/'
     },
     {
       id: 1,
-      image: 'https://vercel.com/_next/image?url=%2Fapi%2Fscreenshot%3Fdark%3D1%26deploymentId%3Ddpl_HjSPgwA6Etfb94QkbfDfBHaj9oUe%26teamId%3Drohits-projects-1ff89b20%26withStatus%3D1&w=1920&q=75&dpl=dpl_Hp88LwrD6GmyCpS3RttV5rbznXBX',
+      image: 'https://vercel.com/_next/image?url=%2Fapi%2Fscreenshot%3Fdark%3D1%26deploymentId%3Ddpl_7Vf2Yu6pjUiVbHonAqNRZAqLqie3%26teamId%3Drohits-projects-1ff89b20%26withStatus%3D1&w=1920&q=75&dpl=dpl_2q5xmLHavNwyLXRhZZV9GC6pWhpd',
       title: 'Personel Portfolio',
       description: ' Web Design',
       link: 'https://personal-portfolio-kappa-teal.vercel.app/'
@@ -103,7 +91,7 @@ const Portfolio = () => {
         </button>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 text-gray-300 h-full p-5 py-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 text-gray-300 h-full p-5 py-5">
         {projects[activeTab].map(project => (
           <a
             key={project.id}
@@ -117,8 +105,8 @@ const Portfolio = () => {
               alt={project.title}
               className="w-full h-48 object-cover"
             />
-            <div className="absolute inset-0 flex flex-col justify-center items-center text-center bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 p-4">
-              <h3 className="font-bold mb-2 text-3xl text-white font-dm-sans">{project.title}</h3>
+            <div className="absolute inset-0 flex flex-col justify-center items-center text-center bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 p-4 border hover:border-white">
+              <h3 className="font-bold mb-2 text-3xl bg-gradient-to-r from-violet-500  via-red-600 via-orange-500 to-yellow-500 bg-clip-text text-transparent font-dm-sans">{project.title}</h3>
               <p className='font-bold font-dm-sans'>{project.description}</p>
             </div>
           </a>
